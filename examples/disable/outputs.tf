4da8c960d3cdae79bf6ed2bb2df8316cc7e699cf
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2019-07-11 11:01:12 -0400

    test: adds disable example outputs for out resolvers

--- examples/disable/outputs.tf
+++ examples/disable/outputs.tf
@@ -21,3 +21,19 @@ output "route53_resolver_inbound_arns" {
 output "route53_resolver_inbound_host_vpc_ids" {
   value = "${module.disable.resolver_inbound_host_vpc_ids}"
 }
+
+output "route53_resolver_outbound_security_group_id" {
+  value = "${module.disable.zone_public_ids}"
+}
+
+output "route53_resolver_outbound_ids" {
+  value = "${module.disable.zone_public_ids}"
+}
+
+output "route53_resolver_outbound_arns" {
+  value = "${module.disable.zone_public_ids}"
+}
+
+output "route53_resolver_outbound_host_vpc_ids" {
+  value = "${module.disable.zone_public_ids}"
+}
