ae89cb51fef6cf609c4bd8dd93115b5602f187b2
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2019-07-10 20:34:56 -0400

    test: updates disable example

--- examples/disable/outputs.tf
+++ examples/disable/outputs.tf
@@ -5,3 +5,19 @@ output "route53_zone_private_ids" {
 output "route53_zone_public_ids" {
   value = "${module.disable.zone_public_ids}"
 }
+
+output "route53_resolver_inbound_security_group_id" {
+  value = "${module.disable.zone_public_ids}"
+}
+
+output "route53_resolver_inbound_ids" {
+  value = "${module.disable.zone_public_ids}"
+}
+
+output "route53_resolver_inbound_arns" {
+  value = "${module.disable.zone_public_ids}"
+}
+
+output "route53_resolver_inbound_host_vpc_ids" {
+  value = "${module.disable.zone_public_ids}"
+}
